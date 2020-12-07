6ce55e080986b1ef33181e745c2bfd3ad5a5afad
mihas <mkoypish@gmail.com>
2020-12-07 16:26:55 +0300

    timeout

--- dj_admin_changelist_charts/admin.py
+++ dj_admin_changelist_charts/admin.py
@@ -2,49 +2,58 @@
 import json
 
 from django.contrib.admin.views.main import ChangeList
+from django.db import transaction, connection
 from django.db.models import Count
 from django.contrib import admin
 
 
 class ChangeListMixin(ChangeList):
 
-    chart_group_by = None
 
     def get_results(self, request):
         super(ChangeListMixin, self).get_results(request)
 
-        def change_none_to_str(value):
+        def _change_none_to_str(value):
             if value is None:
-                return 'Value is not specified'
+                return self.model_admin.chart_null_value_text
+            if value is '':
+                return self.model_admin.chart_empty_value_text
             return value
 
-        self.piechardata = json.dumps([
+        with transaction.atomic(), connection.cursor() as cursor:
+            if self.model_admin.chart_sql_execution_time_limit_msec:
+                cursor.execute('SET LOCAL statement_timeout TO %s;' % self.model_admin.chart_sql_execution_time_limit_msec)
 
-            {'name': change_none_to_str(x[self.chart_group_by]), 'y': x['count']}
-            for x in self.queryset.all().order_by(self.chart_group_by).values(
-                self.chart_group_by
-            ).annotate(count=Count('*'))
-        ]
-        )
+            attr = self.model_admin.chart_group_by_attr
+
+            self.piechardata = json.dumps([
+
+                {'name': _change_none_to_str(x[attr]), 'y': x['count']}
+                for x in self.queryset.all().order_by(attr).values(
+                    attr
+                ).annotate(count=Count('*'))
+            ])
 
 
 class AdminWIthChartsMixin(admin.ModelAdmin):
     def get_changelist_instance(self, request):
         return super().get_changelist_instance(request)
 
-    chart_group_by = None
+    chart_group_by_attr = None
+    chart_sql_execution_time_limit_msec = 0 # Enable it for postgres. Sqlite doesn't support `SET LOCAL statement_timeout TO`
+    chart_null_value_text = 'Value is not specified'
+    chart_empty_value_text = 'Value is empty string'
+
 
     def get_changelist(self, request, **kwargs):
-        assert self.chart_group_by is not None, 'chart_group_by should be set to model admin instance'
+        assert self.chart_group_by_attr is not None, 'chart_group_by should be set to model admin instance'
 
         cls =  super().get_changelist(request, **kwargs)
 
         return type(
             cls.__name__ + '_WithCharts',
             (ChangeListMixin, cls),
-            {
-                'chart_group_by': self.chart_group_by,
-            }
+            {}
         )
 
 
